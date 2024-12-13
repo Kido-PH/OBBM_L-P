94dfdf5e0aaa28e8eb54b7c9e26886c102cea5af
BachQuocThinh <137755552+BachQuocThinh@users.noreply.github.com>
2024-12-13 15:17:40 +0700

    update nhánh

--- frontend/src/views/Admin-DashboardPage.js
+++ frontend/src/views/Admin-DashboardPage.js
@@ -10,14 +10,16 @@ import {
   MenuOutlined,
   FileDoneOutlined,
   UnorderedListOutlined,
-  ShoppingCartOutlined,
   AppstoreOutlined,
   ForkOutlined,
+  BellOutlined,
+  ShoppingCartOutlined,
 } from "@ant-design/icons";
 import { Outlet, Route, Routes, useNavigate } from "react-router-dom";
 import { Col, Layout, Menu, Row, theme } from "antd";
 import ManageContracts from "../components/Admin/Admin-Contracts";
 import AdminUserAvatar from "components/Admin/Admin-UserAvatar";
+import { CoffeeOutlined } from "@mui/icons-material";
 
 const { Header, Sider, Content, Footer } = Layout;
 
@@ -31,7 +33,6 @@ function DashboardPage() {
     token: { colorBgContainer },
   } = theme.useToken();
 
-<<<<<<< HEAD
   // Xử lý khi cuộn trang
   useEffect(() => {
     const handleScroll = () => {
@@ -51,27 +52,36 @@ function DashboardPage() {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-=======
-  const userRole = localStorage.getItem("roles") ? JSON.parse(localStorage.getItem("roles")).name : null;
->>>>>>> main
+
+  const userRole = localStorage.getItem("roles")
+    ? JSON.parse(localStorage.getItem("roles")).name
+    : null;
 
   const menuItems = [
-
-<<<<<<< HEAD
+    {
+      key: "1",
+      icon: <BarChartOutlined />,
+      label: "TỔNG QUAN",
+      path: "/admin",
+    },
+    {
+      key: "2",
+      icon: <FileDoneOutlined />,
+      label: "HỢP ĐỒNG",
+      path: "/admin/ManageContracts",
+    },
     {
       key: "3",
       icon: <UnorderedListOutlined />,
-      label: "DANH MỤC",
+      label: "DANH MỤC MÓN ĂN",
       path: "/admin/ManageCategoryDish",
     },
-
     {
       key: "4",
       icon: <ForkOutlined />,
       label: "MÓN ĂN",
       path: "/admin/ManageDish",
     },
-
     {
       key: "5",
       icon: <CustomerServiceOutlined />,
@@ -90,12 +100,8 @@ function DashboardPage() {
       label: "ĐỊA ĐIỂM",
       path: "/admin/ManageLocation",
     },
-    {
-      key: "8",
-      icon: <UsergroupAddOutlined />,
-      label: "KHÁCH HÀNG",
-      path: "/admin/ManageAccounts",
-    },
+    // Các mục chỉ dành cho ADMIN
+
     {
       key: "9",
       icon: <AppstoreOutlined />,
@@ -104,38 +110,26 @@ function DashboardPage() {
     },
     {
       key: "10",
-      icon: <ShoppingCartOutlined  />,
+      icon: <ShoppingCartOutlined />,
       label: "NGUYÊN LIỆU",
       path: "/admin/ManagerIngredient",
     },
-    {
-      key: "11",
-      icon: <SisternodeOutlined />,
-      label: "PHÂN QUYỀN",
-      path: "/admin/AccessControl",
-    },
-=======
-
-    { key: "1", icon: <BarChartOutlined />, label: "TỔNG QUAN", path: "/admin" },
-    { key: "2", icon: <FileDoneOutlined />, label: "HỢP ĐỒNG", path: "/admin/ManageContracts" },
-    { key: "3", icon: <UnorderedListOutlined />, label: "DANH MỤC MÓN ĂN", path: "/admin/ManageCategoryDish" },
-    { key: "4", icon: <CoffeeOutlined />, label: "MÓN ĂN", path: "/admin/ManageDish" },
-    { key: "5", icon: <CustomerServiceOutlined />, label: "DỊCH VỤ", path: "/admin/ManageServices" },
-    { key: "6", icon: <StarOutlined />, label: "SỰ KIỆN", path: "/admin/ManageEvents" },
-    { key: "7", icon: <EnvironmentOutlined />, label: "ĐỊA ĐIỂM", path: "/admin/ManageLocation" },
-    // Các mục chỉ dành cho ADMIN
-    
-    { key: "9", icon: <MenuOutlined />, label: "THỰC ĐƠN", path: "/admin/MenuManagement" },
-    { key: "10", icon: <BellOutlined />, label: "NGUYÊN LIỆU", path: "/admin/ManagerIngredient" },
     ...(userRole === "ADMIN"
       ? [
-          { key: "8", icon: <UsergroupAddOutlined />, label: "KHÁCH HÀNG", path: "/admin/ManageAccounts" },
-          { key: "11", icon: <SisternodeOutlined />, label: "PHÂN QUYỀN", path: "/admin/AccessControl" },
+          {
+            key: "8",
+            icon: <UsergroupAddOutlined />,
+            label: "KHÁCH HÀNG",
+            path: "/admin/ManageAccounts",
+          },
+          {
+            key: "11",
+            icon: <SisternodeOutlined />,
+            label: "PHÂN QUYỀN",
+            path: "/admin/AccessControl",
+          },
         ]
       : []),
-    { key: "12", icon: <LogoutOutlined />, label: "ĐĂNG XUẤT", onclick: handleLogout },
-    
->>>>>>> main
   ];
   return (
     <Layout>
